b300430e760e07def537893f74c8870555569a6b
leeseungjun <cjstkrak@gmail.com>
2023-11-26 15:52:10 +0900

    add: Workspaces 모듈 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { LoggerMiddleware } from './middlewares/logger.middleware';
 import { UsersModule } from './users/users.module';
 import { DmsModule } from './dms/dms.module';
 import { ChannelsModule } from './channels/channels.module';
+import { WorkspacesModule } from './workspaces/workspaces.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), UsersModule, DmsModule, ChannelsModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), UsersModule, DmsModule, ChannelsModule, WorkspacesModule],
   controllers: [AppController],
   providers: [AppService, ConfigService],
 })
