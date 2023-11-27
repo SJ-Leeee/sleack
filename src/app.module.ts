359943cd8f499f0087306ee5ca57dda81a1e13c6
leeseungjun <cjstkrak@gmail.com>
2023-11-27 23:58:57 +0900

    fix: env ignore 재적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { UsersModule } from './users/users.module';
 import { DmsModule } from './dms/dms.module';
 import { ChannelsModule } from './channels/channels.module';
 import { WorkspacesModule } from './workspaces/workspaces.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -15,6 +16,20 @@ import { WorkspacesModule } from './workspaces/workspaces.module';
     DmsModule,
     ChannelsModule,
     WorkspacesModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: process.env.MYSQL_USERNAME,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+      entities: ['entities/*.{js,ts}'],
+      synchronize: true,
+      keepConnectionAlive: true,
+      logging: true,
+      charset: 'utf8mb4',
+      // 이모티콘 쓰기 위해
+    }),
   ],
   controllers: [AppController],
   providers: [AppService, ConfigService],
